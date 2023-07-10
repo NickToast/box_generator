94126a64478ab5ca2ef774ea4cbe6e273d210929
NickToast <nicholastangsouvanh@gmail.com>
2023-07-10 12:38:00 -0700

    First Commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+const Form = () => {
+
+    const [boxColor, setBoxColor] = useState("");
+
+    
+
+
+
+
+
+
+    return (
+        <div className='StyledBox'></div>
+    )
+}
+
+export default Form
